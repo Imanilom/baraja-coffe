b39d30be9cbc3fb9798da58b2924ecf38c43a164
Rilo Supriyatno <rilobahtiar21@gmail.com>
2025-04-07 00:54:48 +0700

    Oauth data login profile

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -186,3 +186,26 @@ export const getUSerById = async (req, res) => {
     res.status(500).json({ message: error.message });
   }
 }
+
+export const getUserProfile = async (req, res) => {
+  const authHeader = req.headers.authorization;
+
+  if (!authHeader || !authHeader.startsWith('Bearer ')) {
+    return res.status(401).json({ message: 'Token tidak ditemukan' });
+  }
+
+  const token = authHeader.split(' ')[1];
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    const user = await User.findById(decoded.id).select('-password'); // Hindari return password
+
+    if (!user) {
+      return res.status(404).json({ message: 'User tidak ditemukan' });
+    }
+
+    res.status(200).json({ user });
+  } catch (err) {
+    res.status(401).json({ message: 'Token tidak valid' });
+  }
+};
