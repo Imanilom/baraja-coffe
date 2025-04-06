ada36a6c4ba6362a4c463075072f7cb0bff3f578
Rilo Supriyatno <rilobahtiar21@gmail.com>
2025-04-07 00:54:48 +0700

    Oauth data login profile

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -4,13 +4,32 @@ import {
   updateUser,
   deleteUser,
   getUSerById,
+  getUserProfile,
 } from '../controllers/user.controller.js';
 import { verifyToken } from '../utils/verifyUser.js';
+const authMiddleware = (req, res, next) => {
+  const authHeader = req.headers.authorization;
+
+  if (!authHeader || !authHeader.startsWith('Bearer ')) {
+    return res.status(401).json({ message: 'Token tidak tersedia' });
+  }
+
+  const token = authHeader.split(' ')[1];
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = decoded; // Simpan user ID ke req.user
+    next();
+  } catch (err) {
+    res.status(401).json({ message: 'Token tidak valid' });
+  }
+};
 
 const router = express.Router();
 
 router.get('/', verifyToken(["customer", "admin", "superadmin"]), test);
 router.get('/getUSerById/:id', getUSerById);
+router.get('/profile', authMiddleware, getUserProfile);
 router.post('/update/:id', verifyToken(["customer", "admin", "superadmin"]), updateUser);
 router.delete('/delete/:id', verifyToken(["customer", "admin", "superadmin"]), deleteUser);
 
