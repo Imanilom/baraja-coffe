6e2759f74c0b83c757658c9a36a10a7040a0607d
Rilo Supriyatno <rilobahtiar21@gmail.com>
2025-04-21 15:49:47 +0700

    update order

--- api/models/Payment.model.js
+++ api/models/Payment.model.js
@@ -1,7 +1,7 @@
 import mongoose from 'mongoose';
 
 const PaymentSchema = new mongoose.Schema({
-  order_id: { type: String, required: true },
+  order_id: { type: mongoose.Schema.Types.ObjectId, ref: 'Order' },
   amount: { type: Number, required: true },
   paymentDate: { type: Date, default: Date.now },
   paymentMethod: { type: String, enum: ['Cash', 'Card', 'E-Wallet', 'Debit'], required: true },
