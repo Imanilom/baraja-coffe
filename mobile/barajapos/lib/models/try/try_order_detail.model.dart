9c61599d7ba5225cfbaa4f031c1d47b79ff6a45b
Bagus Farhan Kirana <bagusfk08@gmail.com>
2025-05-04 16:01:50 +0700

    online order bisa tampil

--- mobile/barajapos/lib/models/try/try_order_detail.model.dart
+++ mobile/barajapos/lib/models/try/try_order_detail.model.dart
@@ -2,29 +2,29 @@
 
 import 'package:freezed_annotation/freezed_annotation.dart';
 import 'package:hive_ce/hive.dart';
-import 'package:barajapos/models/adapter/order_item.model.dart';
+// import 'package:barajapos/models/adapter/order_item.model.dart';
+import 'package:barajapos/models/try/try_order_item.model.dart';
 
-part 'order_detail.model.freezed.dart';
-part 'order_detail.model.g.dart';
+part 'try_order_detail.model.freezed.dart';
+part 'try_order_detail.model.g.dart';
 
 @freezed
-@HiveType(typeId: 5)
-abstract class OrderDetailModel with _$OrderDetailModel {
-  factory OrderDetailModel({
+@HiveType(typeId: 9)
+abstract class TryOrderDetailModel with _$TryOrderDetailModel {
+  factory TryOrderDetailModel({
     @HiveField(0) @JsonKey(name: 'userId') String? customerId,
     @HiveField(1) String? customerName,
     @HiveField(2) String? cashierId,
     @HiveField(3) String? phoneNumber,
-    @HiveField(4) @Default([]) List<OrderItemModel> items,
+    @HiveField(4) @Default([]) List<TryOrderItemModel> items,
     @HiveField(5) required String orderType,
     @HiveField(6) String? deliveryAddress,
     @HiveField(7) String? tableNumber,
     @HiveField(8) String? paymentMethod,
     @HiveField(9) String? status,
     @HiveField(10) double? totalPrice,
-    @HiveField(11) @JsonKey(name: '_id') String? orderId,
-  }) = _OrderDetailModel;
+  }) = _TryOrderDetailModel;
 
-  factory OrderDetailModel.fromJson(Map<String, dynamic> json) =>
-      _$OrderDetailModelFromJson(json);
+  factory TryOrderDetailModel.fromJson(Map<String, dynamic> json) =>
+      _$TryOrderDetailModelFromJson(json);
 }
--- mobile/barajapos/lib/repositories/online_order_repository.dart
+++ mobile/barajapos/lib/repositories/online_order_repository.dart
@@ -1,5 +1,6 @@
 import 'package:barajapos/services/order_service.dart';
 import 'package:barajapos/models/adapter/order_detail.model.dart';
+// import 'package:barajapos/models/try/try_order_detail.model.dart';
 
 class OnlineOrderRepository {
   final OrderService _orderService = OrderService();
@@ -10,8 +11,10 @@ class OnlineOrderRepository {
       print("response pending orders: $response");
       print("Data pending orders yg diambil: ${response.length}");
 
-      final onlineOrders =
-          response.map((json) => OrderDetailModel.fromJson(json)).toList();
+      final onlineOrders = response
+          .take(10)
+          .map((json) => OrderDetailModel.fromJson(json))
+          .toList();
       return onlineOrders;
     } catch (e) {
       print("Gagal mengambil data pending orders: ${e.toString()}");
