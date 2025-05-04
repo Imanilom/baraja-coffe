c2bcb6e772d34dbcdc4b1ba3f008e6649a59033c
Bagus Farhan Kirana <bagusfk08@gmail.com>
2025-05-04 19:54:47 +0700

    confirm order berhasil

--- mobile/barajapos/lib/services/order_service.dart
+++ mobile/barajapos/lib/services/order_service.dart
@@ -1,7 +1,9 @@
 import 'package:barajapos/models/adapter/order_detail.model.dart';
+import 'package:barajapos/providers/auth_provider.dart';
 import 'package:barajapos/services/api_response_handler.dart';
 import 'package:dio/dio.dart';
 import 'package:barajapos/configs/app_config.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 class OrderService {
   final Dio _dio = Dio(
@@ -46,6 +48,57 @@ class OrderService {
       return response.data as List;
     } on DioException catch (e) {
       print('error fetch pending orders: ${e.response?.data}');
+      throw ApiResponseHandler.handleError(e);
+    }
+  }
+
+  Future<Map<String, dynamic>> confirmPendingOrder(
+      WidgetRef ref, OrderDetailModel orderDetail) async {
+    final cashier = ref.read(authCashierProvider);
+
+    try {
+      if (orderDetail.orderId == null) {
+        throw Exception("orderId atau cashierId tidak boleh null");
+      }
+      Response response = await _dio.post(
+        '/api/confirm-order',
+        data: {
+          //cashierId dari authCashierProvider
+          'cashierId': cashier.value?.id,
+          'orderId': orderDetail.orderId,
+        },
+        options: Options(
+          headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json',
+          },
+        ),
+      );
+
+      print('response confirm order: ${response.data}');
+
+      return response.data;
+    } on DioException catch (e) {
+      print('error fetch order detail: ${e.response?.data}');
+      throw ApiResponseHandler.handleError(e);
+    }
+  }
+
+  Future<List<dynamic>> fetchOrderHistory() async {
+    try {
+      Response response = await _dio.get(
+        '/api/order-history',
+        options: Options(
+          headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json',
+          },
+        ),
+      );
+
+      return response.data as List;
+    } on DioException catch (e) {
+      print('error fetch order history: ${e.response?.data}');
       throw ApiResponseHandler.handleError(e);
     }
   }
