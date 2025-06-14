6937c4048416ca31d455185ec94255c97b74c29c
Imamilom <memerlin90@gmail.com>
2025-06-14 09:40:01 +0700

    fi

--- api/workers/jobRouter.js
+++ api/workers/jobRouter.js
@@ -1,6 +1,5 @@
 import { createOrderHandler } from './handlers/createOrderHandler.js';
 import { updateInventoryHandler } from './handlers/updateInventoryHandler.js';
-import { createOrderQueue } from '../queues/order.queue.js';
 
 export const jobRouter = {
   create_order: createOrderHandler,
