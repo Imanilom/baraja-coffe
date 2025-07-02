fe7396510542c13de18d6fa33a1e614bebe13073
Imamilom <memerlin90@gmail.com>
2025-07-02 09:19:36 +0700

    perbaikan tex & service

--- api/routes/taxAndService.routes.js
+++ api/routes/taxAndService.routes.js
@@ -14,7 +14,7 @@ const router = express.Router();
 
 router.get('/', getAllCharges);
 router.get('/:id', getChargeById);
-router.post('/', marketingAccess, createCharge);
+router.post('/',  createCharge);
 router.put('/:id', marketingAccess, updateCharge);
 router.delete('/:id', marketingAccess, deleteCharge);
 
