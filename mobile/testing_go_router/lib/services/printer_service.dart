67168884a1079fb5f1fee16ecfeef9756b109dcd
Bagus Farhan Kirana <bagusfk08@gmail.com>
2025-07-30 15:47:25 +0700

    Update BASE_URL and add esc_pos_printer dependency
    Changed BASE_URL in .env to a new ngrok endpoint. Added esc_pos_printer package to pubspec.yaml dependencies. Removed unused cashier.model import from printer_service.dart.

--- mobile/testing_go_router/lib/services/printer_service.dart
+++ mobile/testing_go_router/lib/services/printer_service.dart
@@ -1,7 +1,6 @@
 import 'package:esc_pos_utils_plus/esc_pos_utils_plus.dart';
 import 'package:flutter/services.dart';
 import 'package:kasirbaraja/models/bluetooth_printer.model.dart';
-import 'package:kasirbaraja/models/cashier.model.dart';
 import 'package:kasirbaraja/models/order_detail.model.dart';
 import 'package:kasirbaraja/services/hive_service.dart';
 import 'package:print_bluetooth_thermal/print_bluetooth_thermal.dart';
