b4bbab757e43f0d42918f6d136a8b9c1b075756a
Bagus Farhan Kirana <bagusfk08@gmail.com>
2025-08-19 13:08:49 +0700

    Add outlet info model and enhance tax/service calculations
    Introduces OutletInfoModel with Hive and Freezed integration, updates tax and service models to support outlet-specific applicability, and refactors OrderDetailProvider to use new tax/service calculation logic. Menu items are now sorted alphabetically, and TaxAndServiceRepository provides detailed breakdowns and outlet-aware calculations. Also updates .env BASE_URL.

---
+++ mobile/testing_go_router/lib/models/outlet_info.model.freezed.dart
@@ -0,0 +1,160 @@
+// dart format width=80
+// coverage:ignore-file
+// GENERATED CODE - DO NOT MODIFY BY HAND
+// ignore_for_file: type=lint
+// ignore_for_file: unused_element, deprecated_member_use, deprecated_member_use_from_same_package, use_function_type_syntax_for_parameters, unnecessary_const, avoid_init_to_null, invalid_override_different_default_values_named, prefer_expression_function_bodies, annotate_overrides, invalid_annotation_target, unnecessary_question_mark
+
+part of 'outlet_info.model.dart';
+
+// **************************************************************************
+// FreezedGenerator
+// **************************************************************************
+
+// dart format off
+T _$identity<T>(T value) => value;
+
+/// @nodoc
+mixin _$OutletInfoModel {
+
+@HiveField(0)@JsonKey(name: '_id') String? get id;@HiveField(1) String? get name;@HiveField(2) String? get address;@HiveField(3) String? get city;@HiveField(4) String? get contactNumber;
+/// Create a copy of OutletInfoModel
+/// with the given fields replaced by the non-null parameter values.
+@JsonKey(includeFromJson: false, includeToJson: false)
+@pragma('vm:prefer-inline')
+$OutletInfoModelCopyWith<OutletInfoModel> get copyWith => _$OutletInfoModelCopyWithImpl<OutletInfoModel>(this as OutletInfoModel, _$identity);
+
+  /// Serializes this OutletInfoModel to a JSON map.
+  Map<String, dynamic> toJson();
+
+
+@override
+bool operator ==(Object other) {
+  return identical(this, other) || (other.runtimeType == runtimeType&&other is OutletInfoModel&&(identical(other.id, id) || other.id == id)&&(identical(other.name, name) || other.name == name)&&(identical(other.address, address) || other.address == address)&&(identical(other.city, city) || other.city == city)&&(identical(other.contactNumber, contactNumber) || other.contactNumber == contactNumber));
+}
+
+@JsonKey(includeFromJson: false, includeToJson: false)
+@override
+int get hashCode => Object.hash(runtimeType,id,name,address,city,contactNumber);
+
+@override
+String toString() {
+  return 'OutletInfoModel(id: $id, name: $name, address: $address, city: $city, contactNumber: $contactNumber)';
+}
+
+
+}
+
+/// @nodoc
+abstract mixin class $OutletInfoModelCopyWith<$Res>  {
+  factory $OutletInfoModelCopyWith(OutletInfoModel value, $Res Function(OutletInfoModel) _then) = _$OutletInfoModelCopyWithImpl;
+@useResult
+$Res call({
+@HiveField(0)@JsonKey(name: '_id') String? id,@HiveField(1) String? name,@HiveField(2) String? address,@HiveField(3) String? city,@HiveField(4) String? contactNumber
+});
+
+
+
+
+}
+/// @nodoc
+class _$OutletInfoModelCopyWithImpl<$Res>
+    implements $OutletInfoModelCopyWith<$Res> {
+  _$OutletInfoModelCopyWithImpl(this._self, this._then);
+
+  final OutletInfoModel _self;
+  final $Res Function(OutletInfoModel) _then;
+
+/// Create a copy of OutletInfoModel
+/// with the given fields replaced by the non-null parameter values.
+@pragma('vm:prefer-inline') @override $Res call({Object? id = freezed,Object? name = freezed,Object? address = freezed,Object? city = freezed,Object? contactNumber = freezed,}) {
+  return _then(_self.copyWith(
+id: freezed == id ? _self.id : id // ignore: cast_nullable_to_non_nullable
+as String?,name: freezed == name ? _self.name : name // ignore: cast_nullable_to_non_nullable
+as String?,address: freezed == address ? _self.address : address // ignore: cast_nullable_to_non_nullable
+as String?,city: freezed == city ? _self.city : city // ignore: cast_nullable_to_non_nullable
+as String?,contactNumber: freezed == contactNumber ? _self.contactNumber : contactNumber // ignore: cast_nullable_to_non_nullable
+as String?,
+  ));
+}
+
+}
+
+
+/// @nodoc
+@JsonSerializable()
+
+class _OutletInfoModel implements OutletInfoModel {
+  const _OutletInfoModel({@HiveField(0)@JsonKey(name: '_id') this.id, @HiveField(1) this.name = null, @HiveField(2) this.address = null, @HiveField(3) this.city = null, @HiveField(4) this.contactNumber = null});
+  factory _OutletInfoModel.fromJson(Map<String, dynamic> json) => _$OutletInfoModelFromJson(json);
+
+@override@HiveField(0)@JsonKey(name: '_id') final  String? id;
+@override@JsonKey()@HiveField(1) final  String? name;
+@override@JsonKey()@HiveField(2) final  String? address;
+@override@JsonKey()@HiveField(3) final  String? city;
+@override@JsonKey()@HiveField(4) final  String? contactNumber;
+
+/// Create a copy of OutletInfoModel
+/// with the given fields replaced by the non-null parameter values.
+@override @JsonKey(includeFromJson: false, includeToJson: false)
+@pragma('vm:prefer-inline')
+_$OutletInfoModelCopyWith<_OutletInfoModel> get copyWith => __$OutletInfoModelCopyWithImpl<_OutletInfoModel>(this, _$identity);
+
+@override
+Map<String, dynamic> toJson() {
+  return _$OutletInfoModelToJson(this, );
+}
+
+@override
+bool operator ==(Object other) {
+  return identical(this, other) || (other.runtimeType == runtimeType&&other is _OutletInfoModel&&(identical(other.id, id) || other.id == id)&&(identical(other.name, name) || other.name == name)&&(identical(other.address, address) || other.address == address)&&(identical(other.city, city) || other.city == city)&&(identical(other.contactNumber, contactNumber) || other.contactNumber == contactNumber));
+}
+
+@JsonKey(includeFromJson: false, includeToJson: false)
+@override
+int get hashCode => Object.hash(runtimeType,id,name,address,city,contactNumber);
+
+@override
+String toString() {
+  return 'OutletInfoModel(id: $id, name: $name, address: $address, city: $city, contactNumber: $contactNumber)';
+}
+
+
+}
+
+/// @nodoc
+abstract mixin class _$OutletInfoModelCopyWith<$Res> implements $OutletInfoModelCopyWith<$Res> {
+  factory _$OutletInfoModelCopyWith(_OutletInfoModel value, $Res Function(_OutletInfoModel) _then) = __$OutletInfoModelCopyWithImpl;
+@override @useResult
+$Res call({
+@HiveField(0)@JsonKey(name: '_id') String? id,@HiveField(1) String? name,@HiveField(2) String? address,@HiveField(3) String? city,@HiveField(4) String? contactNumber
+});
+
+
+
+
+}
+/// @nodoc
+class __$OutletInfoModelCopyWithImpl<$Res>
+    implements _$OutletInfoModelCopyWith<$Res> {
+  __$OutletInfoModelCopyWithImpl(this._self, this._then);
+
+  final _OutletInfoModel _self;
+  final $Res Function(_OutletInfoModel) _then;
+
+/// Create a copy of OutletInfoModel
+/// with the given fields replaced by the non-null parameter values.
+@override @pragma('vm:prefer-inline') $Res call({Object? id = freezed,Object? name = freezed,Object? address = freezed,Object? city = freezed,Object? contactNumber = freezed,}) {
+  return _then(_OutletInfoModel(
+id: freezed == id ? _self.id : id // ignore: cast_nullable_to_non_nullable
+as String?,name: freezed == name ? _self.name : name // ignore: cast_nullable_to_non_nullable
+as String?,address: freezed == address ? _self.address : address // ignore: cast_nullable_to_non_nullable
+as String?,city: freezed == city ? _self.city : city // ignore: cast_nullable_to_non_nullable
+as String?,contactNumber: freezed == contactNumber ? _self.contactNumber : contactNumber // ignore: cast_nullable_to_non_nullable
+as String?,
+  ));
+}
+
+
+}
+
+// dart format on
