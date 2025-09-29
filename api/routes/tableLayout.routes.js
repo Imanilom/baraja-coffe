f79f46a7edc843a1b04b813e4574c610e9084d50
Rilo Supriyatno <rilobahtiar21@gmail.com>
2025-09-29 16:08:05 +0700

    update tableLayout.controller.js, api/index.js, TableLayout.model.js and tableLayout.routes.js

--- api/routes/tableLayout.routes.js
+++ api/routes/tableLayout.routes.js
@@ -1,38 +1,38 @@
-import express from 'express';
-import {
-  createTableLayout,
-  getTableLayoutByOutlet,
-  updateTableLayout,
-  deleteTableLayout,
-  getTableStatusByOutlet,
-  getAvailableTablesByPeople,
-  updateTableStatus
-} from '../controllers/tableLayout.controller.js';
+// import express from 'express';
+// import {
+//   createTableLayout,
+//   getTableLayoutByOutlet,
+//   updateTableLayout,
+//   deleteTableLayout,
+//   getTableStatusByOutlet,
+//   getAvailableTablesByPeople,
+//   updateTableStatus
+// } from '../controllers/tableLayout.controller.js';
 
-import { verifyToken } from '../utils/verifyUser.js';
+// import { verifyToken } from '../utils/verifyUser.js';
 
-const router = express.Router();
+// const router = express.Router();
 
-const waitersAccess = verifyToken(['staff', 'waiter', 'admin', 'superadmin']);
-// POST /api/table-layout
-router.post('/', waitersAccess, createTableLayout);
+// const waitersAccess = verifyToken(['staff', 'waiter', 'admin', 'superadmin']);
+// // POST /api/table-layout
+// router.post('/', waitersAccess, createTableLayout);
 
-// GET /api/table-layout/:outletId
-router.get('/:outletId', getTableLayoutByOutlet);
+// // GET /api/table-layout/:outletId
+// router.get('/:outletId', getTableLayoutByOutlet);
 
-// PUT /api/table-layout/:outletId
-router.put('/:outletId', waitersAccess, updateTableLayout);
+// // PUT /api/table-layout/:outletId
+// router.put('/:outletId', waitersAccess, updateTableLayout);
 
-// DELETE /api/table-layout/:outletId
-router.delete('/:outletId', waitersAccess, deleteTableLayout);
+// // DELETE /api/table-layout/:outletId
+// router.delete('/:outletId', waitersAccess, deleteTableLayout);
 
-// Ambil status meja berdasarkan outlet
-router.get('/table-status/:outletId', getTableStatusByOutlet);
+// // Ambil status meja berdasarkan outlet
+// router.get('/table-status/:outletId', getTableStatusByOutlet);
 
-// Ambil status meja berdasarkan outlet
-router.get('/available-bypeople/:outletId', getAvailableTablesByPeople);
+// // Ambil status meja berdasarkan outlet
+// router.get('/available-bypeople/:outletId', getAvailableTablesByPeople);
 
-// Update status meja
-router.put('/update-status/:outletId', waitersAccess, updateTableStatus);
+// // Update status meja
+// router.put('/update-status/:outletId', waitersAccess, updateTableStatus);
 
-export default router;
+// export default router;
