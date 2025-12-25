cc202d120de94e5ce3cf651b9f9b8c5b7da05757
Rilo Supriyatno <rilobahtiar21@gmail.com>
2025-12-25 13:50:51 +0700

    update order.controller.js and api/index.js

--- api/index.js
+++ api/index.js
@@ -194,19 +194,19 @@ app.use((err, req, res, next) => {
 // =====================================================
 const startServer = async () => {
   try {
-    // await mongoose.connect(process.env.MONGO, {
-    //   serverSelectionTimeoutMS: 10000, // 10 detik max nunggu Atlas
-    // });
-    // console.log('✅ Connected to MongoDB TEST');
-    await mongoose.connect(process.env.MONGO_PROD, {
+    await mongoose.connect(process.env.MONGO, {
       serverSelectionTimeoutMS: 10000, // 10 detik max nunggu Atlas
     });
-    console.log('warning : Connected to MongoDB PROD ✅');
+    console.log('✅ Connected to MongoDB TEST');
+    // await mongoose.connect(process.env.MONGO_PROD, {
+    //   serverSelectionTimeoutMS: 10000, // 10 detik max nunggu Atlas
+    // });
+    // console.log('warning : Connected to MongoDB PROD ✅');
 
-    setupStockCalibrationCron();
-    // startAutoCancelScheduler();
-    setupPaymentExpiryMonitor();
-    initTableSyncJob(2); // ✅ Run table sync every 2 minutes
+    // setupStockCalibrationCron();
+    // // startAutoCancelScheduler();
+    // setupPaymentExpiryMonitor();
+    // initTableSyncJob(2); // ✅ Run table sync every 2 minutes
     // Jalankan sekali untuk generate secret
     // console.log('Webhook Secret:', generateWebhookSecret());
 
